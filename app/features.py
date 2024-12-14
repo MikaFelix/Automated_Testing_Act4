f0783ad97ffb928f15801c7b5e7431759b38e6b1
MikaFelix <166593035+MikaFelix@users.noreply.github.com>
2024-12-14 22:24:00 +0800

    Delete app directory

--- app/features.py
+++
@@ -1,7 +0,0 @@
-class DarkMode:
-    def __init__(self):
-        self.is_dark_mode = False
-
-    def toggle(self):
-        self.is_dark_mode = not self.is_dark_mode
-        return self.is_dark_mode
--- app/main.py
+++
@@ -1,4 +0,0 @@
-def get_feedback():
-    feedback = input("Enter your feedback: ")
-    print(f"Feedback submitted: {feedback}")
-    return feedback
