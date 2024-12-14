46d82d1be45ee5173b9e097e593f3a6ea6b0c8fd
MikaFelix <166593035+MikaFelix@users.noreply.github.com>
2024-12-14 23:00:29 +0800

    Delete tests directory

--- tests/test_dark_mode.py
+++
@@ -1,6 +0,0 @@
-from app.features import DarkMode
-
-def test_dark_mode_toggle():
-    dark_mode = DarkMode()
-    assert dark_mode.toggle() == True  # Toggle on
-    assert dark_mode.toggle() == False  # Toggle off
